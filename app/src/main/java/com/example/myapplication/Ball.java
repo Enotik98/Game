488eea673ea24245480cb504855ae26de1598b4a
Enotik98 <poliakitti@gmail.com>
2022-12-07 23:27:58 +0200

    Lab3

---
+++ app/src/main/java/com/example/myapplication/Ball.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+public class Ball {
+    public int color;
+    public Position p;
+    public Ball(int color, Position p) {
+        this.color = color;
+        this.p = p;
+    }
+}
